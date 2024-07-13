9c162db071f15d6ee85a341c5ce83318977d982e
bswck <bartoszpiotrslawecki@gmail.com>
2024-07-13 06:07:46 +0200

    Set up framework

---
+++ theme.py
@@ -0,0 +1,7 @@
+import manim
+
+FONT = "Liberation Mono"
+DARK_PYTHON_BLUE = manim.ManimColor("#306998")
+DARKER_PYTHON_BLUE = manim.ManimColor("#306998", alpha=0.5)
+PYTHON_BLUE = BRIGHT_PYTHON_BLUE = manim.ManimColor("#65A4D2")
+PYTHON_YELLOW = manim.ManimColor("#ffe262")
