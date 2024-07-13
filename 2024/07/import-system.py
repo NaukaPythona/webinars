0bc2a4e0b5233b8db05181822d22d95668e17543
bswck <bartoszpiotrslawecki@gmail.com>
2024-07-13 07:55:06 +0200

    Expand slide framework

--- 2024/07/import-system.py
+++ 2024/07/import-system.py
@@ -1,4 +1,6 @@
-#!./present -S Intro
+#!./present Intro Modularity Strategies
+
+from __future__ import annotations
 
 from manim import *
 from manim_slides import Slide
@@ -13,23 +15,33 @@ with lazy_importing():
 
 class Intro(Slide):
     def construct(self) -> None:
-        text = Text(
+        title = theme.title(
             "Jak działają importy w Pythonie?",
-            font=theme.FONT,
+            place=UP,
             color=theme.BRIGHT_PYTHON_BLUE,
         )
-
-        underline = Line(color=theme.DARKER_PYTHON_BLUE)
-        underline.stretch_to_fit_width(text.width)
-        underline.next_to(text, direction=DOWN)
-
-        self.play(
-            GrowFromCenter(text),
-            GrowFromCenter(underline),
-        )
+        underline = theme.underline(title)
+        self.play(theme.title_anim(title, underline))
 
         self.next_slide()
 
+        logotype = theme.logotype()
+        logotype.next_to(underline, direction=DOWN * 2)
+        self.play(FadeIn(logotype))
+
+        self.next_slide()
+
+
+class Modularity(Slide):
+    def construct(self) -> None:
+        self.play(theme.title_anim("1. Problem modularności"))
+        self.next_slide()
+
+
+class Strategies(Slide):
+    def construct(self) -> None:
+        self.play(theme.title_anim("2. Importery"))
+        self.next_slide()
 
 
 if __name__ == "__main__":
--- theme.py
+++ theme.py
@@ -1,7 +1,73 @@
-import manim
+from __future__ import annotations
 
-FONT = "Liberation Mono"
-DARK_PYTHON_BLUE = manim.ManimColor("#306998")
-DARKER_PYTHON_BLUE = manim.ManimColor("#306998", alpha=0.5)
-PYTHON_BLUE = BRIGHT_PYTHON_BLUE = manim.ManimColor("#65A4D2")
-PYTHON_YELLOW = manim.ManimColor("#ffe262")
+import manim
+from slothy import type_importing
+
+with type_importing():
+    from pathlib import Path
+    from typing import Final
+
+    import numpy as np
+
+
+FONT: Final = "Liberation Mono"
+LOGO_PATH: Path = Path("assets/logotype.png")
+
+DARK_PYTHON_BLUE: Final = manim.ManimColor("#306998")
+DARKER_PYTHON_BLUE: Final = manim.ManimColor("#306998", alpha=0.5)
+BRIGHT_PYTHON_BLUE: Final = manim.ManimColor("#65A4D2")
+PYTHON_BLUE: Final = BRIGHT_PYTHON_BLUE
+PYTHON_YELLOW: Final = manim.ManimColor("#ffe262")
+
+
+def underline(
+    mobject: manim.Mobject,
+    color: manim.ManimColor | None = None,
+) -> manim.Line:
+    underline = manim.Line(color=color or DARKER_PYTHON_BLUE)
+    underline.stretch_to_fit_width(mobject.width)
+    underline.next_to(mobject, direction=manim.DOWN)
+    return underline
+
+
+def underline_anim(
+    mobject: manim.Mobject,
+    underline: manim.Line,
+) -> manim.LaggedStart:
+    return manim.LaggedStart(
+        manim.GrowFromCenter(mobject),
+        manim.GrowFromCenter(underline),
+    )
+
+
+def title(
+    content: str,
+    place: np.ndarray = manim.UP * 3,
+    color: manim.ManimColor | None = None,
+) -> manim.Text:
+    title = manim.Text(
+        content,
+        font=FONT,
+        color=color or DARK_PYTHON_BLUE,
+    )
+    title.move_to(place)
+    return title
+
+
+def title_anim(
+    title_text_or_mobject: manim.Text | str,
+    underline_mobject: manim.Line | None = None,
+) -> manim.LaggedStart:
+    if isinstance(title_text_or_mobject, str):
+        title_mobject = title(title_text_or_mobject)
+    else:
+        title_mobject = title_text_or_mobject
+    if underline_mobject is None:
+        underline_mobject = underline(title_mobject)
+    return underline_anim(title_mobject, underline_mobject)
+
+
+def logotype(scale: float = 0.6) -> manim.ImageMobject:
+    image = manim.ImageMobject(LOGO_PATH)
+    image.scale(scale)
+    return image
